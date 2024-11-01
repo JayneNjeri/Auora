1334e29dcf91827d8c2248a3f59ca076ecc2ba89
JayneNjeri <jaynenjeri739@gmail.com>
2024-11-01 13:12:04 +0300

    Fetching the API OMDB for movies and series and statically adding one part of a show to display it on the web

--- Movie_Area/src/App.jsx
+++ Movie_Area/src/App.jsx
@@ -1,38 +1,57 @@
-import { useState } from 'react';
-import './App.css'
+import { useState, useEffect } from 'react';
+import './index.css';
+import SearchIcon from './assets/search.svg';
 
-// const Person =(props)=>{
-//   //Props allow you to pass data from a parent (wrapping) component to a child (embedded) component.
+  //Api key 7fb6c640
+const API_URL="http://www.omdbapi.com/?apikey=7fb6c640&";
 
-//   return(
-//     <>
-//     <h1>Name:{props.name}</h1>
-//     <h2>Age:{props.age}</h2>
-//     <h3>Last Name: {props.lastname}</h3>
-//     </>
-    
-//   )
-// }
+const movie1=
+  {
+    "Title": "Batman Begins",
+    "Year": "2005",
+    "imdbID": "tt0372784",
+    "Type": "Movie",
+    "Poster": "https://m.media-amazon.com/images/M/MV5BODIyMDdhNTgtNDlmOC00MjUxLWE2NDItODA5MTdkNzY3ZTdhXkEyXkFqcGc@._V1_SX300.jpg"
+}
+
 
 function App() {
-  const [counter, setCounter]=useState(0);
+  // useEffect(() => {
+  //   searchMovies("Batman");
+  // }, []);
+
   return (
-  
       <div className="App">
-       {/* <Person name={'Jane' } age={25} lastname= {'Mwangi'}/>
-      <Person name={'John' } age={30} lastname= {'Doe'}/>
-       <Person/> */}
+        <h1>MovieLand</h1>
 
-       <button onClick={()=>setCounter((prevCount)=>prevCount -1)}>-</button>
+        <div className="search">
+          <input placeholder="Search for Movies" value="Batman"
+          onChange={()=>{}}
+          />
+          <img src={SearchIcon} alt="Search"
+          onClick={()=>{}}
+           />
+          </div>
 
-       <h1>{counter}</h1>
+          <div className='Container'>
+            <div className='Movie'>
+              <div>
+                <p>{movie1.Year}</p>
+              </div>
+              <div>
+                <img src={movie1.Poster!=='N/A'? movie1.Poster:'https://via.placeholder.com/400'} alt={movie1.Title} />
+              </div>
+              <div>
+                <span>{movie1.Type}</span>
+                <h3>{movie1.Title}</h3>
+              </div>
+            </div>
 
-       <button  onClick={()=>setCounter((prevCount)=>prevCount +1)}>+</button>
-      
-        </div>
-      
-  
+
+          </div>
+
+        </div> 
   )
 }
 
-export default App
+export default App;
